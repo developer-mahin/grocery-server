de588a67fce6a30f068b96ecac7a91da1c175754
developer-mahin <mdmahin1310@gmail.com>
2023-04-07 00:56:16 +0600

    create two api for getting all categories and categories of product

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const app = express();
 const cors = require("cors")
 const port = process.env.PORT || 5000
 
+
+const categories = require("./Data/categories.json")
+const products = require("./Data/products.json")
+
 app.use(express.json())
 app.use(cors())
 
+app.get("/categories", async (req, res) => {
+    res.send(categories)
+})
 
+app.get("/products", async (req, res) => {
+    res.send(products)
+})
 
-
-
+app.get("/category/:id", async (req, res) => {
+    const id = req.params.id;
+    const selectedCategory = products.filter(product => product.category_id === id)
+    res.send(selectedCategory)
+})
 
 
 app.get("/", async (req, res) => {
