d7c3dd2b67a0ac0b5b6101d728555fe011ae5b35
developer-mahin <mdmahin1310@gmail.com>
2023-04-08 16:26:51 +0600

    create get api for getting single product

--- index.js
+++ index.js
@@ -10,19 +10,30 @@ const products = require("./Data/products.json")
 app.use(express.json())
 app.use(cors())
 
+// get api for getting all category
 app.get("/categories", async (req, res) => {
     res.send(categories)
 })
 
+// get api for getting all products
 app.get("/products", async (req, res) => {
     res.send(products)
 })
 
+// get api for getting categories of products
 app.get("/category/:id", async (req, res) => {
     const id = req.params.id;
     const selectedCategory = products.filter(product => product.category_id === id)
     res.send(selectedCategory)
 })
+
+// get api fro getting a single product
+app.get("/booking-product/:id", async (req, res) => {
+    const id = req.params.id;
+    const product = products.find(product => product._id === id)
+    res.send(product)
+})
+
 
 
 app.get("/", async (req, res) => {
